d7f0e0e701d8e7de47eefa273d76896ab5e6a900
Garv274 <garv.malik2010@gmail.com>
2026-09-17 09:44:59 -0500

    sdsd

--- dialogExample/src/app/index.tsx
+++ dialogExample/src/app/index.tsx
@@ -1,9 +1,11 @@
 import { Text, View, StyleSheet } from "react-native";
 import { PaperProvider } from "react-native-paper";
+import DialogTitle from "react-native-paper/lib/typescript/components/Dialog/DialogTitle";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Text>This is an example of the title</Text>
+      <DialogTitle />
     </View>
   );
 }
