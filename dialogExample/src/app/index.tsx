60f377aa204add5450a5fde67c55b7742ef9625d
Garv274 <garv.malik2010@gmail.com>
2026-09-18 09:47:10 -0500

    button works

--- dialogExample/src/app/index.tsx
+++ dialogExample/src/app/index.tsx
@@ -1,23 +1,15 @@
 import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Dialog, Portal } from "react-native-paper";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import DialogTitle from "../../components/DialogTitle";
 
-const [visible, setVisible] = useState(false);
-
-const hideDialog = () => setVisible(false);
 export default function Index() {
+  const [visible, setVisible] = useState(false);
   return (
     <View style={styles.container}>
-      return (
-      <Portal>
-        <Dialog visible={visible} onDismiss={hideDialog}>
-          <Dialog.Title>This is a title</Dialog.Title>
-          <Dialog.Content>
-            <Text>This is simple dialog</Text>
-          </Dialog.Content>
-        </Dialog>
-      </Portal>
-      );
+      <TouchableOpacity onPress={() => setVisible(!visible)}>
+        {visible && <DialogTitle />}
+        <Text>show Dialog.title</Text>
+      </TouchableOpacity>
     </View>
   );
 }
